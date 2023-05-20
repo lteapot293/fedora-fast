88069a443fc83584664db47a0d185b75562afc5c
lteapot293 <123525317+lteapot293@users.noreply.github.com>
2023-05-20 20:27:58 +0300

    Add files via upload

---
+++ fedora.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo &
+sudo dnf install --nogpgcheck https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm &
+flatpak install flathub com.mattjakeman.ExtensionManager &
+sudo dnf rm firefox &
+sudo dnf in google-chrome git gcc make cmake &
+sudo dnf install gnome-tweaks &
+sudo dnf copr enable avm/mutter-patched &
+sudo dnf --refresh upgrade &
+git clone https://github.com/ryanoasis/nerd-fonts
